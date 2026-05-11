7de49b18a6d08ff5221c09cb75df2a8230831f05
sukma <sukmawatikhrsm216@gmail.com>
2026-05-12 00:56:12 +0700

    ini sukma lagi

--- src/projectkos/ProjectKos.java
+++ src/projectkos/ProjectKos.java
@@ -14,7 +14,7 @@ public class ProjectKos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // hai ini sukma lagi 
     }
     
 }
